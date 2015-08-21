c1b4802edff346a77d8c3fabe26b584f2a174cd4
waponx <18826419224@163.com>
2015-08-21 17:47:52 +0800

    对数据库的整体设计做了大幅的修改，让CACWGroup和CACWTask表参照CACWUser表，并且在CACWTask新增了TaskID属性

--- server/CACWDB.sql
+++ server/CACWDB.sql
@@ -1,23 +1,27 @@
-create table CACWGroup(
-	GroupName varchar(20) not null,
-	groupid set("1","2","3","4","5","6","7", "8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32") not null primary key,
-	groupAdmin bigint not null)engine=innodb, default charset=utf8;
 create table CACWUser(
-	groupid bigint not null,
 	UserName varchar(20) not null,
 	UserPassword varchar(20) not null,
-	Userid bigint not null primary key auto_increment,
-	MyGroup set("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32") not null,
-	sex enum("male", "female"),
-	email varchar(50),
-	address varchar(100),
+	UserID bigint not null primary key auto_increment,
+	Sex enum("male", "female"),
+	Email varchar(50),
+	Address varchar(100),
 	PhoneNumber char(10),
-	shortPhoneNumber varchar(10)
-)engine=innodb, default charset=utf8;
+	ShortPhoneNumber varchar(10))engine=innodb, default charset=utf8;
+create table CACWGroup(
+	GroupID bigint primary key auto_increment,
+	GroupName varchar(20) not null,
+	GroupAdmin bigint null,
+	GroupLeader bigint not null,
+	FOREIGN KEY (GroupAdmin) REFERENCES CACWUser(UserID),
+	FOREIGN KEY (GroupLeader) REFERENCES CACWUser(UserID)
+	)engine=innodb, default charset=utf8;
 create table CACWTask(
-	Userid bigint not null,
-	TaskName varchar(50) not null primary key,
+	UserID bigint not null,
+	TaskID bigint primary key  auto_increment,
+	TaskName varchar(50) not null,
 	TaskDestription text,
-	starttime datetime not null,
-	endtime datetime not null
+	Starttime datetime not null,
+	Endtime datetime not null,
+	FOREIGN KEY (UserID) REFERENCES CACWUser(UserID),
+	CONSTRAINT Task_UserID_FK FOREIGN KEY (UserID) REFERENCES CACWUser(UserID)
 )engine=innodb, default charset=utf8;
