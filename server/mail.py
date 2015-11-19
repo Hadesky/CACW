62a87eb954d05913a9b7759f65d1e19c207fc04f
waponx <18826419224@163.com>
2015-11-19 16:19:48 +0800

    修改了发邮件的ｐｙｔｈｏｎ脚本，从本地发送邮件

--- server/mail.py
+++ server/mail.py
@@ -1,7 +1,31 @@
+#import sys
+#import httplib2
+
+#urlstr = 'http://dzyone.sinaapp.com/email.php'
+#httpclient = httplib2.Http('.cache')
+
+#res, content = httpclient.request(urlstr, "POST", headers={'email':sys.argv[1], 'code':sys.argv[2]})
+
+#!/usr//bin/python
+#coding=<utf-8>
 import sys
-import httplib2
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
-urlstr = 'http://dzyone.sinaapp.com/email.php'
-httpclient = httplib2.Http('.cache')
+sender = '453589103@qq.com'
+receiver = sys.argv[1]
+subject = 'CACW authcode'
+smtpserver = 'smtp.qq.com'
+username = '453589103@qq.com'
+password = 'testmail'
+content = 'welcome to register CACW account!\n\r\tyour authcode:' + sys.argv[2]
 
-res, content = httpclient.request(urlstr, "POST", headers={'email':sys.argv[1], 'code':sys.argv[2]})
+msg = MIMEText(content, "text", "utf-8")
+msg['Subject'] = Header(subject, 'utf-8')
+
+smtp = smtplib.SMTP()
+smtp.connect('smtp.qq.com')
+smtp.login(username, password)
+smtp.sendmail(sender, receiver, msg.as_string())
+smtp.quit()
