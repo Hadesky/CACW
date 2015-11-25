bb6f3d1147a25a86805a0c7bae46212577a9d491
waponx <18826419224@163.com>
2015-11-25 15:25:25 +0800

    使用了C++链接php服务器

--- server/httpregisteraction.cc
+++ server/httpregisteraction.cc
@@ -31,14 +31,17 @@ namespace {
 	//	从地址中获取套接字，如果成功链接就返回对应的套接字，如果没有就返回-1
 	int Socket(AddrType *res) {
 		int fd;
-		bool reuseaddr = true;
+//		bool reuseaddr = true;
 		while (res != NULL) {
 			fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 			if (fd > 0) {
-				if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(bool))  == 0) {
-					if (connect(fd, res->ai_addr, sizeof(struct sockaddr)) == 0){
-						return true;
-					}
+//				if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(bool))  == 0) {
+//					if (connect(fd, res->ai_addr, sizeof(struct sockaddr)) == 0){
+//						return fd;
+//					}
+//				}
+				if (connect(fd, res->ai_addr, sizeof(struct sockaddr)) == 0) {
+					return fd;
 				}
 				close(fd);
 				fd = -1;
@@ -47,6 +50,44 @@ namespace {
 		}
 
 		return fd;
+	}
+	//  修改php服务的地址，如果flag为true则释放空间，返回NULL
+	//	如果地址（res）为NULL，就重新获取php服务器地址
+	AddrType *DoAddr(bool flag) {
+		static struct addrinfo *res = NULL;
+		if (flag == true) {
+			if (res != NULL) {
+				freeaddrinfo(res);
+			}
+			return NULL;
+		}
+		if (res != NULL) {
+			return res;
+		}
+		AddrType *p = new AddrType;
+		if (p == NULL) {
+			return NULL;
+		}
+		memset(p, 0, sizeof(AddrType));
+		//p->ai_flags = AI_CANONNAME;
+		p->ai_family = AF_UNSPEC;
+		p->ai_socktype = SOCK_STREAM;
+
+		int n = getaddrinfo("dzyone.sinaapp.com", "80", p, &res);
+		if (n != 0) {
+			printf("addrinfo error: %s\n", gai_strerror(n));
+			if (res != NULL) {
+				freeaddrinfo(res);
+			}
+			return NULL;
+		}
+		if (NULL == res) {
+#ifdef DEBUG 
+			printf("HttpRegisterAction::Start_rtn_sendmail:\n addr pionter is NULL \n");
+#endif 
+		}
+
+		return res;
 	}
 }
 
@@ -57,6 +98,7 @@ HttpRegisterAction::HttpRegisterAction()
 
 HttpRegisterAction::~HttpRegisterAction() {
 	delete _atctree;
+	DoAddr(true);
 }
 
 bool HttpRegisterAction::Init(boost::shared_ptr<SimpleMySql> spmysql_ptr) {
@@ -113,8 +155,8 @@ std::string HttpRegisterAction::GetAuthCode(const std::string &email) {
 		//	往AuthCode数据结构中插入数据
 		return std::string("001");
 	}
-	std::string command = "python mail.py " + email + " " + code;
-	system(command.c_str());
+//	std::string command = "python mail.py " + email + " " + code;
+//	system(command.c_str());
 	
 //	Msg *msg = new Msg(email, code);
 //	pthread_t ptid;
@@ -124,37 +166,24 @@ std::string HttpRegisterAction::GetAuthCode(const std::string &email) {
 //#endif
 //		return std::string("001");
 //	}
+	Msg *msg = new Msg(email, code);
+	Start_rtn_sendmail((void *)msg);
+	delete msg;
 
 	return std::string("000");
 }
 
 void *HttpRegisterAction::Start_rtn_sendmail(void *arg) {
-	//  这个函数没右成功执行
-	pthread_detach(pthread_self());
+//	pthread_detach(pthread_self());
 	Msg *msg = (Msg *) arg;
 	int clientfd;
 
-	AddrType *p = new AddrType;
-	if (p == NULL) {
-		return NULL;
-	}
-	memset(p, 0, sizeof(AddrType));
-	//p->ai_flags = AI_CANONNAME;
-	p->ai_family = AF_UNSPEC;
-	p->ai_socktype = SOCK_STREAM;	
-	struct addrinfo *res = NULL;
-	int n = getaddrinfo("dzyone.sinaapp.com", "80", p, &res);
-	if (n != 0) {
-		return NULL;
-	}
-	if ( (clientfd = Socket(res)) < 0) {
-		freeaddrinfo(res);
+	if ( (clientfd = Socket(DoAddr(false))) < 0) {
 #ifdef DEBUG 
-		printf("HttpRegisterAction::Start_rtn_sendmail:\nconnect error %d\n", clientfd);
-#endif 
+		printf("HttpRegisterAction::Start_rtn_sendmail:\nconnect error \n");
+#endif
 		return NULL;
-	}
-	
+	}	
 //	struct sockaddr_in server_addr;
 //	memset(&server_addr, 0, sizeof(server_addr));
 //	server_addr.sin_family = AF_INET;
@@ -173,14 +202,15 @@ void *HttpRegisterAction::Start_rtn_sendmail(void *arg) {
 //		close(clientfd);
 //	}
 	std::string request("\
-POST http://dzyone.sinaapp.com/email.php HTTP/1.1\r\n\
-Accept-Language:zh-cn\r\n\
-Host: http://dzyone.sinaapp.com\r\n");
+POST /email.php HTTP/1.1\r\n\
+Host: dzyone.sinaapp.com\r\n");
 	const std::string email("email: " + msg->_email + "\r\n");
 	const std::string code("code: " + msg->_authcode + "\r\n");
 	request += email + code;
-	request += "Content-Length: 0";
-
+	request += "Content-Length: 0\r\n\r\n";
+#ifdef DEBUG
+	printf("send to :\n%s", request.c_str());
+#endif 
 	send(clientfd, request.c_str(), request.length(), MSG_CONFIRM);
 
 	return NULL;
